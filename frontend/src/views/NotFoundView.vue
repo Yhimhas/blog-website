ad51380fe9d7a3d1168b39d0f62a6762e3e0be56
Yhimhas <2218429253@qq.com>
2026-09-21 21:54:28 +0800

    style: 更新 404 页返回图标

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import SiteIcon from "../components/SiteIcon.vue";
 import { RouterLink } from 'vue-router'
 </script>
-<template><section class="not-found"><div class="eyebrow">404 / NOT FOUND</div><h1>没有找到这个页面</h1><p>检查一下地址，或者回到首页继续浏览。</p><RouterLink class="primary-link" to="/">返回首页 ↗</RouterLink></section></template>
+<template><section class="not-found"><div class="eyebrow">404 / NOT FOUND</div><h1>没有找到这个页面</h1><p>检查一下地址，或者回到首页继续浏览。</p><RouterLink class="primary-link" to="/">返回首页 <SiteIcon name="arrow" /></RouterLink></section></template>
